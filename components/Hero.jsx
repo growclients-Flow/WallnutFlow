1f462e3407d314ae18a746158f6ae13db34da266
Simmba719 <simmba@gmail.com>
2025-10-31 20:56:42 +0530

    gfctfcf

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -58,7 +58,7 @@ export default function Hero() {
           {/* Placeholder image */}
           <div className="w-[80%] h-[80%] flex items-center justify-center bg-gray-100 rounded-xl border border-gray-300">
             <Image
-              href="/public/previewimage.png"
+              href="../public/previewimage.png"
               alt="Preview diagram placeholder"
               width={600}
               height={300}
